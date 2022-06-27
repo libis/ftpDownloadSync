10850cd856cac66c6dbd799d0213b329ae1bdb8b
Tom Vanmechelen <tom.vanmechelen@libis.be>
2022-06-27 10:49:09 +0200

    sync with mounted disk

--- moveData.rb
+++ moveData.rb
@@ -11,25 +11,31 @@ end
 
 config.config_file="primove_lirias.yml"
 
-#puts config[:ftp_server]
-#puts config[:ftp_server][:host]
-puts config[:file_pattern_to_download]
-puts config[:ftp_dir]
-#puts config[:user]
-
 start_process = DateTime.now
 last_run_updates = Time.parse config[:last_run_updates]
 
-puts "Geting files : #{ config[:ftp_dir] }*.tar.gz"
+# puts config[:ftp_server]
+# puts config[:ftp_server][:host]
+# puts config[:file_pattern_to_download]
+# puts config[:ftp_dir]
+# puts config[:user]
+
+puts "Move records from 'sftp' to elastic_data_in"
+puts "last_run_updates : #{last_run_updates}"
+
+
+
+# puts "Geting files : #{ config[:ftp_dir] }*.tar.gz"
 Dir.glob("#{ config[:ftp_dir] }*.tar.gz").each do |file| 
     if ( last_run_updates < File.mtime(file) ) 
+        puts "move #{file}"
         FileUtils.cp(file, config[:download_dir])
         filegz = Zlib::GzipReader.new(File.open(file, 'rb'))
         Minitar::unpack(filegz, config[:extract_dir])
    end
 end
 
-# config[:last_run_updates] = start_process.to_s
+config[:last_run_updates] = start_process.to_s
 
 
 
